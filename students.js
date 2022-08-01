af904863dc9a46d0fb79e70702dc065a028daf2b
Rondolph S Martin <ronnie@Rondolphs-MacBook-Air.local>
2022-08-01 16:56:45 -0400

    html info

--- students.js
+++ students.js
@@ -6,9 +6,17 @@ const port = 4001
 app.use(cors())
 app.use(express.json())
 
-app.get("/",(req, res)=>{
-    res.send("hello world")
+let students = ["bob", "samantha", "lily", "grace", "jim"]
+
+app.get("/", (req, res) => {
+  let output = "<html><body>" //start
+  // output= output+ "hello there html"//middle
+  for (let i = 0; i < students.length; i++) {
+    output = output + '<li><input type ="checkbox">' + students[i] + "</li>"
+  }
+  output = output + "</ul></body></html>" //end
+  res.send(output)
 })
-app.listen(port,()=>{
-    console.log("hello world")
+app.listen(port, () => {
+  console.log("localhost:4000")
 })
