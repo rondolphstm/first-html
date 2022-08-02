4f38f74852051c83868b65667680594cce3ed03d
Rondolph S Martin <ronnie@Rondolphs-MacBook-Air.local>
2022-08-02 12:14:24 -0400

    new info

--- students.js
+++ students.js
@@ -9,13 +9,17 @@ app.use(express.json())
 let students = ["bob", "samantha", "lily", "grace", "jim"]
 
 app.get("/", (req, res) => {
-  let output = "<html><body>" //start
-  // output= output+ "hello there html"//middle
+  let output = "<html><body> you asked for: " + movietitle +"<br>" //start
+
+  output= output+ "hello there html"//middle
+
+  output = output + "</ul></body></html>" //end
+
+  res.send(output)
+
   for (let i = 0; i < students.length; i++) {
     output = output + '<li><input type ="checkbox">' + students[i] + "</li>"
   }
-  output = output + "</ul></body></html>" //end
-  res.send(output)
 })
 app.listen(port, () => {
   console.log("localhost:4000")
